d6becf3916c12bf26c25415318dcbb6421c91283
VFMR <reichv@ifo.de>
2022-09-24 22:24:32 +0200

    implemented all methods for the predictor

--- weelex/predictor.py
+++ weelex/predictor.py
@@ -4,6 +4,7 @@ from typing import Union, List
 
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 from weelex.cluster_tfidf import ctfidf
 from weelex.tfidf import BasicTfidf
@@ -13,8 +14,8 @@ class PredictionProcessor:
     def __init__(self,
                  data: Union[np.ndarray, pd.Series] = None,
                  embeddings: embeddings.Embeddings = None,
-                 tfidf: BasicTfidf = None,
-                 ctfidf: ctfidf.ClusterTfidfVectorizer = None,
+                 tfidf: Union[str, BasicTfidf] = None,
+                 ctfidf: Union[str, ctfidf.ClusterTfidfVectorizer] = None,
                  relevant_pos: List[str] = ['ADJ', 'ADV', 'NOUN', 'VERB'],
                  min_df: Union[int, float] = 5,
                  max_df: Union[int, float] = 0.95,
@@ -51,12 +52,28 @@ class PredictionProcessor:
         self._n_jobs = n_jobs
 
         if tfidf is not None:
-            self._tfidf = tfidf
+            if isinstance(tfidf, str):
+                self.load_tfidf(tfidf)
+            elif isinstance(tfidf, BasicTfidf):
+                self._tfidf = tfidf
+            else:
+                raise ValueError(f"""
+                    Expected 'tfidf' to be of type 'str' or 'BasicTfidf'.
+                    Got {type(tfidf)} instead.
+                """)
         else:
             self._tfidf = None
 
         if ctfidf is not None:
-            self._ctfidf = ctfidf
+            if isinstance(ctfidf, str):
+                self.load_ctfidf(ctfidf)
+            elif isinstance(ctfidf, ctfidf.ClusterTfidfVectorizer):
+                self._ctfidf = ctfidf
+            else:
+                raise ValueError(f"""
+                    Expected 'ctfidf' to be of type 'str' or 'ClusterTfidfVectorizer'.
+                    Got {type(ctfidf)} instead.
+                """)
         else:
             self._ctfidf = None
 
@@ -85,16 +102,134 @@ class PredictionProcessor:
                     n_jobs=self._n_jobs)
         return vectorizer
 
-    def fit_tfidf(self, data: Union[np.ndarray, pd.Series] = None) -> None:
+    @staticmethod
+    def _checksize(array: np.ndarray) -> None:
+        if len(array.shape) != 1:
+            raise ValueError(f"""
+                Expected 1 dimensional array. Got array of shape {array.shape}.
+            """)
+
+    def _get_usedata(self, data):
         if data is None:
             usedata = self._data
         else:
+            self._checksize()
             usedata = data
+        return usedata
 
+    def fit_tfidf(self, data: Union[np.ndarray, pd.Series] = None) -> None:
+        usedata = self._get_usedata(data)
         if self._tfidf is None:
             self._tfidf = self._instantiate_tfidf()
 
         self._tfidf.fit(usedata)
 
-    def process(self):
-        pass
+    def _vectorize(self, data):
+        return np.array(self._ctfidf.transform(data))
+
+    def fit_ctfidf(self, data: Union[np.ndarray, pd.Series] = None) -> None:
+        usedata = self._get_usedata(data)
+        if self._ctfidf is None:
+            self._ctfidf = self._instantiate_ctfidf()
+        # TODO implement ctfidf fit
+        # self._ctfidf.fit(usedata)
+
+    def save_tfidf(self, path: str) -> None:
+        self._tfidf.save(path)
+
+    def load_tfidf(self, path: str) -> None:
+        tfidf = BasicTfidf()
+        tfidf.load(path)
+        self._tfidf = tfidf
+
+    def save_ctfidf(self, dir: str) -> None:
+        self._ctfidf.save(dir)
+
+    def load_ctfidf(self, path: str) -> None:
+        ctfidf = ctfidf.ClusterTfidfVectorizer()
+        ctfidf.load(path)
+        self._ctfidf = ctfidf
+
+    def transform(self, X):
+        if self._tfidf is None or not self._tfidf.check_fit():
+            self.fit_tfidf(X)
+            self.fit_ctfidf(X)
+        elif self._ctfidf is None:
+            self.fit_ctfidf(X)
+        vects = self._ctfidf.transform(X)
+        return self._tfidf_weighted_embeddings_corpus(vects)
+
+    @staticmethod
+    def _get_word_tfidf_pairs(vect, index2word):
+        """Reorganize the sparse tfidf vector, i.e. one row
+        of the sparse tfidf matrix, into a vector containing
+        only the nonzero elements of the vector.
+
+        Args:
+            vect (row of sparse matrix): One row of the sparse matrix resulting from
+                tfidf vectorization.
+            index2word (dict): Lookup table word index-> word string
+
+        Returns:
+            list: Array containing the terms (str) with nonzero vectors
+            numpy.array: vector containing the nonzero tfidf values
+        """
+        vect_array = vect.toarray()[0]
+        indices = list(np.where(vect_array != 0)[0])
+        words = [index2word[x] for x in indices]
+        nonzero_vects = np.array(vect_array[indices])
+        return words, nonzero_vects
+
+    def _tfidf_weighted_embeddings(self, vect, index2word):
+        """Aggregate embedding vectors of rows as a linear combination with
+        the respective words' Tfidf values as weights (normalized to sum to 1)
+
+        Args:
+            vect (sparse vector): One row of the sparse matrix resulting from tfidf
+                vectorization.
+            index2word (dict): Lookup table word index -> word string
+            wv (dict or gensim.models.Fasttext): Lookup for embedding vectors
+            top_n (int, optional): If int: number of terms to aggregate. Allows
+                to only aggregate the n terms with the highest tfidf values. Defaults to None.
+
+        Returns:
+            numpy.array: vector containing the aggregated word embedding
+        """
+        words, weights = self._get_word_tfidf_pairs(vect, index2word)
+        if self._n_top_clusters:
+            n = min(self._n_top_clusters, len(weights))
+            words = [x for _, x in sorted(zip(weights, words))]
+            weights = [y for y, _ in sorted(zip(weights, words))]
+            words = words[-n:]
+            weights = np.array(weights[-n:])
+        weights = weights / weights.sum()
+        ft_array = np.array([self._embeddings[word] for word in words]).T
+        weighted_sum = ft_array @ weights
+        return weighted_sum
+
+    def _tfidf_weighted_embeddings_corpus(self, vects):
+        """Aggregate the embedding vectors of all rows in the data
+
+        Args:
+            vects (sparse matrix): Result from tfidf vectorization of dataset
+            wv (dict or gensim.models.Fasttext): Lookup for embedding vectors of words
+            tfidf (sklearn.feature_processing.text.TfidfVectorizer instance): The Tfidf Vectorizer instance
+                that has been fitted before.
+            top_n (int, optional): If int: number of terms to aggregate. Allows
+                to only aggregate the n terms with the highest tfidf values. Defaults to None.
+
+        Returns:
+            numpy.array: Matrix of size (len(data) x embedding dimensions)
+        """
+        embedding_shape = len(self._embeddings[self._checkterm])
+        index2word = {i: term for term, i in self._tfidf.vocabulary_.items()}
+
+        print('{} rows to process'.format(vects.shape[0]))
+        allvects = np.zeros((vects.shape[0], embedding_shape))
+        i = 0
+        for vect in tqdm(vects):
+            allvects[i] = self._tfidf_weighted_embeddings(vect, index2word)
+            i += 1
+
+        return allvects
+
