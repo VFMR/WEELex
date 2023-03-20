76a671e817b8f2f6b315f4ae5ae39e06fabd721d
VFMR <reichv@ifo.de>
2023-03-20 15:03:51 +0100

    added docstrings

--- weelex/embeddings.py
+++ weelex/embeddings.py
@@ -1,6 +1,6 @@
+"""Contains the class for embedding vectors.
+"""
 from typing import Union, Tuple, Iterable, overload, List, Dict
-
-# from functools import singledispatch, singledispathmethod
 from zipfile import ZipFile
 
 import numpy as np
@@ -13,9 +13,20 @@ from weelex import lexicon
 
 
 class Embeddings:
+    """Class for embedding vector lookup."""
+
     def __init__(
         self, embedding_dict: Dict[str, float] = None, testvalue="test"
     ) -> None:
+        """
+        Args:
+            embedding_dict (Dict[str, float], optional): Embedding vector
+                lookup object. Defaults to None.
+            testvalue (str, optional): Word that is tested against the
+                embedding vectors. Required for example to infer the
+                dimensionality. Make sure it is included among the provided
+                embedding vectors. Defaults to "test".
+        """
         self.isfiltered = False
         self._testvalue = testvalue
         if embedding_dict is not None:
@@ -30,6 +41,15 @@ class Embeddings:
     def filter_terms(
         self, terms: Union[list, lexicon.Lexicon, np.ndarray, pd.DataFrame, tuple]
     ) -> None:
+        """Filter the embeddings down to a word-vector collection that is
+        required, i.e., is included in the vocabulary. Since WEELex utilizes
+        only a fixed number of terms, removing unnecessary ones preserves disk
+        memory when saving the model.
+
+        Args:
+            terms (Union[list, lexicon.Lexicon, np.ndarray, pd.DataFrame, tuple]):
+                Reduce the embedding lookup to these terms
+        """
         if isinstance(terms, np.ndarray) or isinstance(terms, pd.DataFrame):
             terms_lst = self._flatten_matrix(terms)
         elif isinstance(terms, lexicon.Lexicon):
@@ -46,6 +66,11 @@ class Embeddings:
         self._wv = self.make_wv_from_keys_vectors()
 
     def make_wv_from_keys_vectors(self) -> Dict[str, np.ndarray]:
+        """Create word-vector lookup.
+
+        Returns:
+            Dict[str, np.ndarray]: Word - embedding vector mapping.
+        """
         return {key: value for key, value in zip(self._keys, self._vectors)}
 
     @staticmethod
@@ -92,6 +117,15 @@ class Embeddings:
         return vector
 
     def save_filtered(self, path: str) -> None:
+        """Save the filtered embeddings to disk.
+
+        Args:
+            path (str): Where the vectors are saved.
+
+        Raises:
+            ValueError: if the embedding vectors have not been filtered with
+                the `filter_terms` method before.
+        """
         if self._keys is not None:
             try:
                 np.savez_compressed(path, keys=self._keys, vectors=self._vectors)
@@ -100,15 +134,25 @@ class Embeddings:
         else:
             raise ValueError(
                 """There are no key-vector pairs to be saved yet.
-                    Please apply the filter() method before."""
+                    Please apply the filter_terms method before."""
             )
 
     @property
-    def keys(self):
+    def keys(self) -> List[str]:
+        """Words with available embedding vectors.
+
+        Returns:
+            List[str]: Words
+        """
         return self._keys
 
     @property
-    def dim(self):
+    def dim(self) -> int:
+        """Dimensionality of the embedding vectors
+
+        Returns:
+            int: Dimensionality
+        """
         try:
             result = len(self._wv[self._testvalue])
         except:
@@ -124,6 +168,16 @@ class Embeddings:
         ...
 
     def lookup(self, terms: Union[str, list, np.ndarray, pd.Series]) -> np.ndarray:
+        """Retrieve the embedding vectors for a single word or multiple terms.
+
+        Args:
+            terms (Union[str, list, np.ndarray, pd.Series]): Single word or
+                iterable of words.
+
+        Returns:
+            np.ndarray: Array with the embedding vectors. Has size `dim` for a
+                single term and `(number words, dim)` for an interable of words.
+        """
         if isinstance(terms, str):
             values = self._get_val_from_key(terms)
         else:
@@ -138,7 +192,20 @@ class Embeddings:
     # --------------------------------------------------------------------------
     # classmethods
     @classmethod
-    def load_filtered(cls, path: str, archive: ZipFile = None) -> None:
+    def load_filtered(cls, path: str, archive: ZipFile = None) -> "Embeddings":
+        """Load previously filtered and saved Embeddings object from disk.
+
+        Args:
+            path (str): Location of saved object.
+            archive (ZipFile, optional): Name of zip archive the object is in.
+                Defaults to None.
+
+        Raises:
+            ValueError: If the provided object is not supported.
+
+        Returns:
+            Embeddings: Loaded instance
+        """
         instance = cls()
         if archive is None:
             if isinstance(path, str):
@@ -182,14 +249,35 @@ class Embeddings:
     @classmethod
     def load_vectors(
         cls, path: str, embedding_type: str, fine_tuned: bool = False
-    ) -> None:
+    ) -> "Embeddings":
+        """Instantiate Embeddings with pre-trained FastText or Word2Vec models.
+
+        Args:
+            path (str): Location of the embeddings to load.
+            embedding_type (str): The type of embedding vectors. One of
+                {fasttext, word2vec}.
+            fine_tuned (bool, optional): Whether the embedding vectors have
+                been fine tuned. Matters if `embedding_type='fasttext'` since
+                self-trained or fine tuned FastText models need to be read
+                differently than the official ones from [https://fasttext.cc/].
+
+        Example:
+            >>> embeds = Embeddings.load_vectors('path/to/embeddings')  # doctest: +SKIP
+            ...
+
+        Raises:
+            ValueError: If an invalid `embedding_type` is entered.
+
+        Returns:
+            Embeddings: Embeddings object.
+        """
         if embedding_type == "fasttext":
             if not fine_tuned:
-                inst = cls.load_facebook_vectors(path)
+                inst = cls._load_facebook_vectors(path)
             else:
-                inst = cls.load_finetuned_fasttext(path)
+                inst = cls._load_finetuned_fasttext(path)
         elif embedding_type == "word2vec":
-            inst = cls.load_finetuned_word2vec(path)
+            inst = cls._load_finetuned_word2vec(path)
         else:
             raise ValueError(
                 f"""
@@ -200,21 +288,21 @@ class Embeddings:
         return inst
 
     @classmethod
-    def load_facebook_vectors(cls, path: str) -> None:
+    def _load_facebook_vectors(cls, path: str) -> None:
         inst = cls()
         wv = load_facebook_vectors(path)
         inst._wv = wv
         return inst
 
     @classmethod
-    def load_finetuned_fasttext(cls, path: str) -> None:
+    def _load_finetuned_fasttext(cls, path: str) -> None:
         inst = cls()
         wv = FastText.load(path).wv
         inst._wv = wv
         return inst
 
     @classmethod
-    def load_finetuned_word2vec(cls, path: str) -> None:
+    def _load_finetuned_word2vec(cls, path: str) -> None:
         inst = cls()
         wv = Word2Vec.load(path).wv
         inst._wv = wv
