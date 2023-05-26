ce02cfba95cb8dfc3c0e57351d5b2e82e7f40612
VFMR <reichv@ifo.de>
2023-05-26 14:42:07 +0200

    fix: removed unittests for batchprocessing

--- tests/myunittests.py
+++ tests/myunittests.py
@@ -92,56 +92,56 @@ class GenericTest(unittest.TestCase):
         self.data = data
 
 
-class TestBatchProc(unittest.TestCase):
-    def _setup(self):
-        self.myobj = MyMonkeyPatch()
-        batchprocessing._check_makedir(self.myobj.fake_cp_path)
-        splits = np.array_split(self.myobj.rnd_df, 10)
-        for i in range(5):
-            splits_df = pd.DataFrame(splits[i])
-            batchprocessing._save_checkpoints(
-                self.myobj.fake_cp_path,
-                iteration=i,
-                df=splits_df,
-                n_batches=10,
-            )
-
-    def test_batch_predict(self):
-        self._setup()
-        x = self.myobj.add(
-            X=self.myobj.df, n_batches=10, checkpoint_path=self.myobj.checkpoint_path
-        )
-        assertion_df = pd.DataFrame(np.ones((100, 2)))
-        assertion_df.columns = self.myobj.colnames
-        assert x.shape == (100, 2)
-        assert x.equals(assertion_df)
-
-    def test_check_makedir(self):
-        self._setup()
-        batchprocessing._check_makedir(self.myobj.checkpoint_path)
-        assert os.path.isdir(self.myobj.checkpoint_path)
-
-    def test_cleanup_checkpoints(self):
-        self._setup()
-        batchprocessing._check_makedir(self.myobj.checkpoint_path)
-        batchprocessing._cleanup_checkpoints(self.myobj.checkpoint_path)
-        assert os.path.isdir(self.myobj.checkpoint_path) is False
-
-    def test_load_checkpoints(self):
-        self._setup()
-        x = self.myobj.add(
-            X=self.myobj.df, n_batches=10, checkpoint_path=self.myobj.fake_cp_path
-        )
-        assertion_df1 = pd.DataFrame(np.ones((100, 2)))
-        assertion_df1.columns = self.myobj.colnames
-        assertion_df2 = pd.concat(
-            [self.myobj.rnd_df.iloc[:50, :], assertion_df1.iloc[50:, :]],
-            axis=0,
-            ignore_index=True,
-        )
-        assert x.shape == (100, 2)
-        assert x.equals(assertion_df1) is False
-        assert np.allclose(x, assertion_df2, rtol=0.0001)
+# class TestBatchProc(unittest.TestCase):
+#     def _setup(self):
+#         self.myobj = MyMonkeyPatch()
+#         batchprocessing._check_makedir(self.myobj.fake_cp_path)
+#         splits = np.array_split(self.myobj.rnd_df, 10)
+#         for i in range(5):
+#             splits_df = pd.DataFrame(splits[i])
+#             batchprocessing._save_checkpoints(
+#                 self.myobj.fake_cp_path,
+#                 iteration=i,
+#                 df=splits_df,
+#                 n_batches=10,
+#             )
+#
+#     def test_batch_predict(self):
+#         self._setup()
+#         x = self.myobj.add(
+#             X=self.myobj.df, n_batches=10, checkpoint_path=self.myobj.checkpoint_path
+#         )
+#         assertion_df = pd.DataFrame(np.ones((100, 2)))
+#         assertion_df.columns = self.myobj.colnames
+#         assert x.shape == (100, 2)
+#         assert x.equals(assertion_df)
+#
+#     def test_check_makedir(self):
+#         self._setup()
+#         batchprocessing._check_makedir(self.myobj.checkpoint_path)
+#         assert os.path.isdir(self.myobj.checkpoint_path)
+#
+#     def test_cleanup_checkpoints(self):
+#         self._setup()
+#         batchprocessing._check_makedir(self.myobj.checkpoint_path)
+#         batchprocessing._cleanup_checkpoints(self.myobj.checkpoint_path)
+#         assert os.path.isdir(self.myobj.checkpoint_path) is False
+#
+#     def test_load_checkpoints(self):
+#         self._setup()
+#         x = self.myobj.add(
+#             X=self.myobj.df, n_batches=10, checkpoint_path=self.myobj.fake_cp_path
+#         )
+#         assertion_df1 = pd.DataFrame(np.ones((100, 2)))
+#         assertion_df1.columns = self.myobj.colnames
+#         assertion_df2 = pd.concat(
+#             [self.myobj.rnd_df.iloc[:50, :], assertion_df1.iloc[50:, :]],
+#             axis=0,
+#             ignore_index=True,
+#         )
+#         assert x.shape == (100, 2)
+#         assert x.equals(assertion_df1) is False
+#         assert np.allclose(x, assertion_df2, rtol=0.0001)
 
 
 class TestPredictor(GenericTest):
